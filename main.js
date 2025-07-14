e8032715ed992a460ede8dbd354a3fb41a405b3c
khamdialfin <alfinpml220@gmail.com>
2025-07-14 20:01:34 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Web Development" , "Consultant IT" , "Programmer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
